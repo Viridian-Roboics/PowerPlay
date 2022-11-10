8c3f6f9cfdeb96bb76dd77d4a5986d0467e1197b
Dwalker1000 <25walkerd@gmail.com>
2022-11-10 17:53:36 -0600

    a1 2 lift simplification

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teliops/T1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teliops/T1.java
@@ -25,7 +25,6 @@ public class T1 extends LinearOpMode{
 
     //lift Vars
     private DcMotor L1 = null;
-    private DcMotor L2 = null;
     private double Lspeed = 1;
     private double LMin = 0;
     private double LMax = 0;
@@ -46,11 +45,9 @@ public class T1 extends LinearOpMode{
         BL = hardwareMap.get(DcMotor.class, "BL");
         BR = hardwareMap.get(DcMotor.class, "BR");
         L1 = hardwareMap.get(DcMotor.class, "lift1");
-        L2 = hardwareMap.get(DcMotor.class, "lift2");
         LServo = hardwareMap.get(Servo.class, "LServo");
 
         L1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        L2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         FL.setDirection(DcMotor.Direction.REVERSE);
         FR.setDirection(DcMotor.Direction.FORWARD);
@@ -58,7 +55,6 @@ public class T1 extends LinearOpMode{
         BR.setDirection(DcMotor.Direction.FORWARD);
 
         L1.setDirection(DcMotor.Direction.REVERSE);
-        L2.setDirection(DcMotor.Direction.FORWARD);
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
@@ -73,10 +69,10 @@ public class T1 extends LinearOpMode{
             double lateral = gamepad1.left_stick_x;
             double yaw = gamepad1.right_stick_x;
 
-            double FLP  = axial + lateral + yaw;
+            double FLP = axial + lateral + yaw;
             double FRP = axial - lateral - yaw;
-            double BLP   = axial - lateral + yaw;
-            double BRP  = axial + lateral - yaw;
+            double BLP = axial - lateral + yaw;
+            double BRP = axial + lateral - yaw;
 
             max = Math.max(Math.abs(FLP), Math.abs(FRP));
             max = Math.max(max, Math.abs(BLP));
@@ -90,43 +86,24 @@ public class T1 extends LinearOpMode{
             }
 
             //lift
-            if (gamepad1.right_bumper){
-                LTarget = 0;
-                L1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-                L2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-                L1.setPower(Lspeed);
-                L2.setPower(Lspeed);
-                telemetry.addData("Status", "Going up");
-            }
-            else if (gamepad1.left_bumper){
-                LTarget = 0;
-                L1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-                L2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-                L1.setPower(-Lspeed);
-                L2.setPower(-Lspeed);
-                telemetry.addData("Status", "Going down");
-            }
-            else if(!SetPos){
+            if (gamepad1.right_bumper) {
+                RegMoveLift(1, "Going Up");
+            } else if (gamepad1.left_bumper) {
+                RegMoveLift(-1, "Going Down");
+            } else if (LTarget == 0 || LTarget == L1.getCurrentPosition()) {
                 L1.setPower(0);
-                L2.setPower(0);
             }
 
-            if(gamepad1.dpad_up && L1.getCurrentPosition() != TopLift){
+            if (gamepad1.dpad_up && L1.getCurrentPosition() != TopLift) {
                 LTarget = MoveLift(TopLift);
-            }
-            else if(gamepad1.dpad_left && L1.getCurrentPosition() != MiddleLift){
+            } else if (gamepad1.dpad_left && L1.getCurrentPosition() != MiddleLift) {
                 LTarget = MoveLift(MiddleLift);
-            }
-            else if(gamepad1.dpad_down && L1.getCurrentPosition() != BottomLift){
+            } else if (gamepad1.dpad_down && L1.getCurrentPosition() != BottomLift) {
                 LTarget = MoveLift(BottomLift);
-            }
-            else if(gamepad1.dpad_right && L1.getCurrentPosition() != ConeLift){
+            } else if (gamepad1.dpad_right && L1.getCurrentPosition() != ConeLift) {
                 LTarget = MoveLift(ConeLift);
             }
-            if (LTarget != 0){
-                FL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                FR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                L1.setPower(Lspeed);
+            if (LTarget != 0) {
                 L1.setPower(Lspeed);
             }
 
@@ -151,8 +128,15 @@ public class T1 extends LinearOpMode{
         }
     }
     private int MoveLift(int GoalPos){
+        FL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        FR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         L1.setTargetPosition(GoalPos);
-        L2.setTargetPosition(GoalPos);
         return GoalPos;
+    }
+    private void RegMoveLift(int down, String status){
+        LTarget = 0;
+        L1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        L1.setPower(Lspeed * down);
+        telemetry.addData("Status", status);
     }
 }
