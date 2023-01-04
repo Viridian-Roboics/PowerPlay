1bbad03bae8d4c6333e7e23847e96f16884d78cb
MagicisCoding <112664054+MagicisCoding@users.noreply.github.com>
2023-01-04 17:32:18 -0600

    h

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/Actualauton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/Actualauton.java
@@ -59,7 +59,7 @@ public class Actualauton extends LinearOpMode {
     static final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_INCHES * 3.1415);
     static final double DRIVE_SPEED = 0.7;
 
-    //gryo
+    //gry
     private BNO055IMU imu = null;
     private double robotHeading  = 0;
     private double headingOffset = 0;
