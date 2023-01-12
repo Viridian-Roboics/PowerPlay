9156d9bd6331e9fd86037e7646c2174edb124ed8
Dwalker1000 <25walkerd@gmail.com>
2023-01-12 17:41:46 -0600

    update + CV auton

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/TServo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/TServo.java
@@ -1,11 +1,13 @@
 package org.firstinspires.ftc.teamcode.Testing;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 // lift1     lift2
+@Disabled
 @TeleOp(name="Servo Test")
 public class TServo extends LinearOpMode{
     //general Vars
